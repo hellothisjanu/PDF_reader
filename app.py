742a38967650c962cc3882c4938ec5882695f06a
Janani K <138442715+hellothisjanu@users.noreply.github.com>
2025-09-02 18:18:53 +0530

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from transformers import pipeline
